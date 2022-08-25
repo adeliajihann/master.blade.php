c2aa246ed158671b08f0e3c5937f4ac26544eeb3
adeliajihann <102372659+adeliajihann@users.noreply.github.com>
2022-08-25 18:28:16 +0700

    Update index.blade.php

--- index.blade.php
+++ index.blade.php
@@ -62,14 +62,14 @@
         <div class="text-center">
           <img src="{{ asset('template/dist/img/user.png') }}" class="img-responsive">
           </div>
-            <a style="text-align:center">Pegawai</a>
+            <a>Pegawai</a>
           </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-1">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="pages/calendar.html" class="nav-link">
+            <a href="{{url ('pegawai/master')}}" class="nav-link">
               <i class="nav-icon fa fa-th-large"></i>
               <p>
                 Dashboard
@@ -78,7 +78,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="pages/calendar.html" class="nav-link">
+            <a href="{{url ('pegawai/index')}}" class="nav-link">
               <i class="nav-icon fa fa-suitcase"></i>
               <p>
                 Kinerja Pegawai
@@ -104,7 +104,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="pages/calendar.html" class="nav-link">
+            <a href="{{url ('pegawai/settings')}}" class="nav-link">
               <i class="nav-icon fa fa-cog"></i>
               <p>
                 Settings
@@ -129,7 +129,9 @@
           <div class="col-sm-12">
             <h1 class="m-0">Data Kinerja Pegawai</h1>
             <div class="box-header text-right">
-                <a class="btn btn-primary"><i class="fa fa-plus-circle"></i> Tambah Kinerja</a>
+              <a href="{{url ('/pegawai/tambah')}}" class="nav-link"> 
+                <button type="button" class="btn btn-primary"><i class="fa fa-plus-circle"></i> Tambah Kinerja</button>
+              </a>
             </div>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -141,14 +143,15 @@
         <div class="card-header">
             <h3 class="card-title">Show Entries</h3>
 
-            <div class="card-tools">
+            <div class="card-tools" style="padding-top: 10px;">
                 <h3 class="card-title">Search: </h3>
-                <div class="input-group input-group-sm" style="width: 200px;">
+                <div class="input-group input-group-sm" style="width: 200px; padding-left:19px;">
                     <input type="text" name="table_search" class="form-control float-right">
                 </div>
             </div>
         </div>
         <!-- /.card-header -->
+
         <div class="card-body">
           <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"></div><div class="col-sm-12 col-md-6"></div></div><div class="row"><div class="col-sm-12"><table id="example2" class="table table-bordered table-hover dataTable dtr-inline" aria-describedby="example2_info">
             <thead>
@@ -173,29 +176,16 @@
                 </div>
               </td>
             </tr>
-            <tr class="even">
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              <td>
-                <div class="d-grid gap-2 d-md-block" style="text-align:center">
-                    <button class="btn btn-warning" type="button"><i class="fas fa-edit"></i></button>
-                    <button class="btn btn-danger" type="button"><i class="fa fa-trash"></i></button>
-                </div>
-              </td>
-            </tr>
             </tbody>
           </table>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12 col-md-5">
+        <div class="col-sm-9 col-md-9">
             <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Showing 1 to 10 of 30 entries
             </div>
         </div>
-          <div class="col-sm-12 col-md-7">
+          <div class="col-sm-3 col-md-3" style="padding-left: 20px;">
               <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
                     <ul class="pagination">
                         <li class="paginate_button page-item previous disabled" id="example2_previous">
