678fceb0efcd0544b704d9ee6f033a55bfd38c38
adeliajihann <102372659+adeliajihann@users.noreply.github.com>
2022-09-18 14:47:46 +0700

    Update main.blade.php

--- revisi-spiner/main.blade.php
+++ revisi-spiner/main.blade.php
@@ -4,6 +4,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="{{ asset('template/dist/img/logo.png') }}">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+  
   @include('p_part.asset')
 </head>
 @include('p_part.header')
